2901f6b8484701b53cbdce0e078caf8bda5eb910
Vic <324165877@qq.com>
2024-06-17 15:35:30 +0800

    Change Default Character Class

--- Source/GASDemo/GASDemoGameMode.cpp
+++ Source/GASDemo/GASDemoGameMode.cpp
@@ -7,7 +7,7 @@
 AGASDemoGameMode::AGASDemoGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/GASDemo/Blueprints/BP_DemoCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
